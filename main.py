7d69dc46884d2e136d02f2719e42f08ce8bd046b
gingin33 <82872408+gingin33@users.noreply.github.com>
2022-05-28 23:16:34 +0900

    リアルタイム取得
    リアルタイムでTwitterの検索ワードを取得できるようにしました.

---
+++ main.py
@@ -0,0 +1,43 @@
+from logging.config import listen
+import tweepy
+from tweepy import StreamingClient, StreamRule
+import datetime
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+AK = os.getenv('API_KEY')
+AKS = os.getenv('API_KEY_SECRET')
+AT = os.getenv('ACCESS_TOKEN')
+ATS = os.getenv('ACCESS_TOKEN_SECRET')
+BT = os.getenv('BEARER_TOKEN')
+
+
+class TweetPrinter(tweepy.StreamingClient):
+    
+    def on_tweet(self, tweet):
+        print(f"{tweet.id} {tweet.created_at} ({tweet.author_id}): {tweet.text}")
+        print("-"*50)
+       
+try:
+    printer = TweetPrinter(BT)
+    rule_ids = []
+    result = printer.get_rules()
+
+    for rule in result.data:
+        print(f"Rule marked to delete: {rule.id}: {rule.value}")
+        rule_ids.append(rule.id)
+
+    if len(rule_ids) > 0:
+        printer.delete_rules(rule_ids)
+        printer = TweetPrinter(BT)
+        print("Deleted all rules.")
+    else:
+        print("No rules to delete.")
+
+    printer.add_rules(StreamRule(value="プリマジ"))
+    printer.filter()
+except KeyboardInterrupt as e:
+        print(" The stream has been disconnected.")
+
+ 
